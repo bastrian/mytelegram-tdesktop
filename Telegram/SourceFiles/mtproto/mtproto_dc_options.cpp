a9e81c67749cfb395d6b4c3f7182469a23f76d56
bastrian <1041566+bastrian@users.noreply.github.com>
2026-08-12 19:56:31 +0000

    Configure desktop client for 194.104.114.97

--- Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
+++ Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -28,8 +28,8 @@ struct BuiltInDc {
 	int port;
 };
 
-#define IpV4 "192.168.1.100"
-#define IpV6 "192.168.1.100"
+#define IpV4 "194.104.114.97"
+#define IpV6 "194.104.114.97"
 #define IpV4Port 20443
 #define IpV4Port2 20443
 #define IpV6Port 20443
